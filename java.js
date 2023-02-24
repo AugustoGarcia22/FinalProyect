f536ed609d9bc33d2c4d2c10b619cdca3ed45dbd
AugustoGarcia22 <124534144+AugustoGarcia22@users.noreply.github.com>
2023-02-24 02:09:37 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,10 +1,16 @@
 const navToggle = document.querySelector(".nav-boton")
 const navMenu = document.querySelector(".Nav-menu")
+const cerrar = document.querySelector("#Nav-menu")
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("Nav-menu_visible")
 })
 
+cerrar.addEventListener("click", () => {
+    navMenu.classList.remove("Nav-menu_visible");
+})
+
+//*DarkMode*//
 const bdark = document.querySelector('#bdark');
 const body = document.querySelector('body');
 
